949cca0a8c8222c907f62961243616b8063fdf89
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-06 11:57:24 +0600

    Create first query Getting videos on home page

--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,9 +1,36 @@
+import { useGetVideosQuery } from "../../features/api/apiSlice";
+import VideoLoader from "../ui/loaders/VideoLoader";
 import Video from "./Video";
+import Error from "../ui/Error";
 
 export default function Videos() {
-    return (
-        <div className="col-span-12 sm:col-span-6 md:col-span-3 duration-300 hover:scale-[1.03]">
-            <Video />
-        </div>
+  const { data: videos, isLoading, isError } = useGetVideosQuery();
+
+  // decide what to render
+  let content = null;
+
+  if (isLoading) {
+    content = (
+      <>
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+      </>
     );
+  }
+
+  if (!isLoading && isError) {
+    content = <Error message="There was an error" />;
+  }
+
+  if (!isLoading && !isError && videos?.length === 0) {
+    content = <Error message="No videos found!" />;
+  }
+
+  if (!isLoading && !isError && videos?.length > 0) {
+    content = videos.map((video) => <Video key={video.id} video={video} />);
+  }
+
+  return content;
 }
--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -6,6 +6,10 @@ export const apiSlice = createApi({
     baseUrl: "http://localhost:9000",
   }),
   endpoints: (builder) => ({
-
+    getVideos: builder.query({
+      query: () => "/videos",
+    }),
   }),
 });
+
+export const { useGetVideosQuery } = apiSlice;
